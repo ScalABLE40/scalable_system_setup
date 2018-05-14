4ed5b40c08eba3dd9b5aca1c0d402bb37ae9b2ef
francesco <francesco@m-tech.aau.dk>
2018-05-14 13:32:31 +0200

    Updated script for updating workspace

--- scripts/update_workspace.sh
+++ scripts/update_workspace.sh
@@ -5,15 +5,23 @@ function print_info()
     echo "$(tput setaf 6)***** $* *****$(tput sgr0)"
 }
 
+setup_package_dir=$(cd $(dirname "$0"); pwd)
+workspace_source_dir="${setup_package_dir}/../../.." 
+conf_file="$workspace_source_dir/.rosinstall.conf" 
+
+if [[ -z "$1" && ! -f $conf_file ]]
+  then echo "Must specify a rosinstall file the first time you use this script."
+  exit 0
+fi
+
 if [ -z "$1" ]
-  then installfile="scalable.rosinstall"
-  else installfile=$1
+  then installfile=$(cat $conf_file)
+  else 
+    installfile=$1
+    echo $1 > $conf_file
 fi
 
 echo "Updating with: " $installfile
-
-setup_package_dir=$(cd $(dirname "$0"); pwd)
-workspace_source_dir="${setup_package_dir}/../../.." 
 
 print_info "UPDATING SETUP REPOSITORY"
 cd ${setup_package_dir} && git pull
